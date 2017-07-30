1fd7c20e31acdff2e7024290d33289cd628c99f7
Tim <tim@timcollins.ie>
2017-07-30 16:24:43 +0100

    Added callbacks to reduce indentation.

--- src/01-easy-greeting/greeting.js
+++ src/01-easy-greeting/greeting.js
@@ -1,38 +1,21 @@
 var readline = require('readline').createInterface(process.stdin, process.stdout);
 var name, age, username;
 
+readline.question('Name: ', nameCallback);
 
-readline.question('Name: ', (answer) => {
+function nameCallback(answer) {
     name = answer;
-    console.log('Entered', name);
+    readline.question('Age: ', ageCallback);
+}
 
-    readline.question('Age: ', (answer) => {
-        age = answer;
-        console.log('Entered', age);
+function ageCallback(answer) {
+    age = answer;
+    readline.question('Username: ', usernameCallback);
+}
 
-        readline.question('Username: ', (answer) => {
-            username = answer;
-            console.log(`Your name is ${name}, you are ${age} years old, and your username is ${username}`);
+function usernameCallback(answer) {
+    username = answer;
 
-            process.exit(0);
-        });
-    });
-});
-
-// This probably isn't the right way to declare a callback.
-//readline.question('Name: ', nameCallback);
-
-//var nameCallback = (name) => {
-//    name = answer;
-//    console.log('Entered', answer);
-//    process.exit(0);
-//}
-
-//readline.question('Age: ', (answer) => {
-//    age = answer;
-//});
-
-//readline.question('Username: ', (answer) => {
-//    username = answer;
-//    console.log(`Your name is ${name}, you are ${age} years old, and your username is ${username}`);
-//});
+    console.log(`Your name is ${name}, you are ${age} years old, and your username is ${username}.`);
+    process.exit(0);
+}
