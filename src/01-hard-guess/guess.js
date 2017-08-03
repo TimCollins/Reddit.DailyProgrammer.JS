9790871683d346c59f8ce7ed2b72ae06ac5656ad
Tim <tim@timcollins.ie>
2017-08-03 20:08:48 +0100

    Made the code actually work.

--- src/01-hard-guess/guess.js
+++ src/01-hard-guess/guess.js
@@ -4,7 +4,7 @@ var readline = require('readline').createInterface(process.stdin, process.stdout
 
 var max = 100;
 var min = 1;
-var guess = 50;
+var guess = parseInt((min + max) / 2);
 var turns = 0;
 
 // See https://stackoverflow.com/questions/24464404/how-to-readline-infinitely-in-node-js
@@ -13,10 +13,12 @@ var recursiveAsyncReadLine = function () {
 
     readline.question(':> ', function (answer) {
         if (answer === 'h') {
+            min = guess;
             guess = parseInt((guess + max) / 2);
             turns++;
         } else if (answer === 'l') {
-            guess = parseInt((min + guess) / 2);
+            max = guess;
+            guess = parseInt((guess + min) / 2);
             turns++;
         } else if (answer === 'e') {
             console.log('Guessed the correct number in', turns, 'turns.');
