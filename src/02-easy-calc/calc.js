c8a4409b8680094ec0801aff4a2e72d1578426b8
Tim <tim@timcollins.ie>
2017-08-03 21:03:49 +0100

    Tidied up the implementation.

--- src/02-easy-calc/calc.js
+++ src/02-easy-calc/calc.js
@@ -2,12 +2,13 @@
 // This will take two dates and return the number of business days between them.
 
 var start = new Date(Date.parse('2017-07-01'));
-var end = new Date(Date.parse('2017-07-31'));
+var end = new Date(Date.parse('2017-08-01'));
 
 var getBusinessDays = function (start, end) {
     var days = Math.round(Math.abs((start.getTime() - end.getTime()) / (24 * 60 * 60 * 1000)));
     var businessDays = 0;
-    var date = new Date(start.getDate());
+    var date = new Date(Date.parse(start));
+
     for (var i = 0; i < days; i++) {
         var d = date.getDay();
 
@@ -21,6 +22,4 @@ var getBusinessDays = function (start, end) {
     return businessDays;
 };
 
-var businessDays = getBusinessDays(start, end);
-
-console.log('There are', businessDays, 'business days between', start, 'and', end);
+console.log('There are', getBusinessDays(start, end), 'business days between', start, 'and', end);
